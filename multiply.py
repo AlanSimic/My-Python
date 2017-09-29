01830540b5f9795513d3a121264943758e60a09f
AlanSimic <alan.crownstone@gmail.com>
2017-09-29 10:43:43 +0200

    Updated multiplications

--- multiply.py
+++ multiply.py
@@ -8,3 +8,14 @@ c = (a + b)
 print(c + a)
 print(a * 10)
 print('I ' + 'H' + str(8) + ' Mondays')
+
+d = 6
+e = 6
+f = 6
+print(d + e + f)
+g = (d + e + f)
+print(g)
+print(g + 92)
+h = (g + 92)
+print(h)
+print("That " + "will " + "be " + str(h) + " euros")
