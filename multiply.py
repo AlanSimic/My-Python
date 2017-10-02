95ebbb36d39d6fbe1f85a6bad909ac358815f3ba
AlanSimic <alan.crownstone@gmail.com>
2017-10-02 10:07:13 +0200

    Opdracht 2

--- multiply.py
+++ multiply.py
@@ -1,9 +1,14 @@
 a = float(input("Insert a number here : "))
+m = input("Do u add, subtract, multiply or divide? : ")
 b = float(input("Insert a second number here : "))
-
-print(a * b)
-
-
+if m == 'add':
+    print(a + b)
+if m == 'subtract':
+    print(a - b)
+if m == 'multiply':
+    print(a * b)
+if m == 'divide':
+    print(a / b)
 
 
 
