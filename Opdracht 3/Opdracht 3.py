4d65fa17e0031e8ed0568d997a39cbe9929a2ce3
AlanSimic <alan.crownstone@gmail.com>
2017-10-10 11:22:25 +0200

    Opdracht 3

---
+++ Opdracht 3/Opdracht 3.py
@@ -0,0 +1,69 @@
+input("To stop the program, answer 'Do u want to add, subtract, multiply or divide?' with q"", to begin, press enter!")
+a = float(input("Insert a number here : "))
+m = input("Do u want to add, subtract, multiply or divide? : ")
+if m == "q":
+    quit(print("Program ended"))
+b = float(input("Insert a second number here : "))
+
+if m == 'add':
+    result = a + b
+if m == 'subtract':
+    result = a - b
+if m == 'multiply':
+    result = a * b
+if m == 'divide':
+    result = a / b
+
+print(result)
+
+while m != True:
+    p = (input("Do u want to add, subtract, multiply or divide? : "))
+
+    if p == "q":
+            quit(print("Program ended"))
+            break
+    y = float(input("Insert another number here : "))
+    if p == "add":
+        result = result + y
+    if p == "subtract":
+        result = result - y
+    if p == "multiply":
+        result = result * y
+    if p == "divide":
+        result = result / y
+
+
+
+
+    print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Opdracht 3/go to loop.py
@@ -0,0 +1,29 @@
+
+
+(x in ["add", "subtract", "multiply" or "divide"]):
+        print(float(input("Insert another number here : ")))
+
+
+if (input("Do u want to add, subtract, multiply or divide? : ")) != True:
+
+
+
+
+x = ["add", "subtract", "multiply", "divide"]
+
+
+
+o = [(a + b),
+     (a - b),
+     (a * b),
+     (a / b)]
+
+
+print(float(input("Insert another number here : ")))
+
+
+
+s = (input("Do u want to stop? : "))
+        if s == "no":
+            if s == "yes":
+                print("Program stopped")
