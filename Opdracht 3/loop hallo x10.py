329f415f13b5e668aeddb03bee9bd2a7df5ffa3a
AlanSimic <alan.crownstone@gmail.com>
2017-10-10 11:24:53 +0200

    Opdracht 3

---
+++ Opdracht 3/loop hallo x10.py
@@ -0,0 +1,7 @@
+a = input("Insert a word: ")
+#if n > a; then stop
+#if n <= a; then don't stop
+n = 1
+while str(n) > a:
+    print(a)
+    a = a + n
---
+++ Opdracht 3/wtf.py
@@ -0,0 +1,8 @@
+a = int(input("Insert a number: "))
+#if n > a; then stop
+#if n <= a; then don't stop
+n = 1
+while (n <= a):
+    print(n)
+    n = n + 1
+# n <= a is now false
